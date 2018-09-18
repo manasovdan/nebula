842459d7dcdfb255dced91fbba77e94dc922f93b
Daniel Manasov <daniel@manasov.xyz>
2018-09-18 18:09:05 +0600

    Change directory structure

--- src/api/routes/api.js
+++ src/api/routes/api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.post('/reviews', (req, res, next) => {
+router.post('/reviews', (req, res) => {
   res.status(200).json({ test: 'success' });
 });
 
